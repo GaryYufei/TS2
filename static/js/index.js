490195fcc5137b005a9abda235fd277ad7f4bdbb
GaryYufei <garyyufei@gmail.com>
2015-04-06 18:07:18 +1000

    add more

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
+	
+	if ((!('webkitSpeechRecognition' in window)) || (!('webkitSpeechRecognition' in window))) {
+		$("#status").text("please use Google chrome");
+		return;
+	} else {
+		$("#status").text("Good");
+	}
+
 	events = {};
 	events.domainAnswerEvent = "domain-name";
 	events.initConceptsAnswerEvent = "init-concepts-questions";
 	events.comfirmDifferentAnswerEvent = "comfirm-different-answer";
+	events.nextConcept = "next-concept";
+	events.newConcept = "new-concept";
 	voices = window.speechSynthesis.getVoices();
+
+	var domain_name;
+	var queue = [];
+	var init_concepts;
+	var current_concepts;
 
 	function ask(words, recog, e) {
 		recog.onresult = function(event) {
@@ -34,13 +49,11 @@ $(document).ready(function() {
         speechSynthesis.speak(u1);
 	}
 
-	if ((!('webkitSpeechRecognition' in window)) || (!('webkitSpeechRecognition' in window))) {
-		$("#status").text("please use Google chrome");
-	} else {
-		$("#status").text("Good");
+	function ask_concept(recog) {
+		current_concepts = queue.shift();
+		var question = "What do " + current_concepts.cA + " and " + current_concepts.cB + " have in common that " + current_concepts.d + " does not have";
+		ask(question,recog,events.newConcept);
 	}
-
-	
 
 	var recognition = new webkitSpeechRecognition();
 	recognition.continuous = true;
@@ -48,17 +61,35 @@ $(document).ready(function() {
 
 	$(window).on(events.domainAnswerEvent, function(e,d){
 		var question = "Name three concepts that are important in the context of ";
-		ask(question + d.answer, recognition, events.initConceptsAnswerEvent);
+		domain_name = d.answer;
+		ask(question + domain_name, recognition, events.initConceptsAnswerEvent);
 	});
 
 	$(window).on(events.initConceptsAnswerEvent, function(e,d){
 		var question = "Are these different concepts";
+		init_concepts = d.answer.split(" ");
 		ask(question, recognition, events.comfirmDifferentAnswerEvent);
+	});
+
+	$(window).on(events.nextConcept, function(e){
+		ask_concept(recognition);
+	});
+
+	$(window).on(events.newConcept, function(e,d){
+		queue.push({"cA":current_concepts.cA,"cB":d.answer,"d":current_concepts.cB});
+		queue.push({"cA":current_concepts.cB,"cB":d.answer,"d":current_concepts.cA});
+		$(window).trigger(events.nextConcept);
 	});
 
 	$(window).on(events.comfirmDifferentAnswerEvent, function(e,d){
 		if(d.answer.toLowerCase() == "yes") {
-			alert("yes");
+			queue.push({"cA":init_concepts[0],"cB":init_concepts[1],"d":init_concepts[2]});
+			queue.push({"cA":init_concepts[2],"cB":init_concepts[1],"d":init_concepts[0]});
+			queue.push({"cA":init_concepts[0],"cB":init_concepts[2],"d":init_concepts[1]});
+			$(window).trigger(events.nextConcept);
+		} else {
+			var question = "Name three concepts that are important in the context of ";
+			ask(question + domain_name, recognition, events.initConceptsAnswerEvent);
 		}
 	});
 
